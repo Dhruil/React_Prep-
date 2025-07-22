3d0b9d83096d7e6a5d3083b4fc44125905b8014a
dhruil parmar <parmardhruil2611@gmail.com>
2025-07-22 15:13:31 +0530

    update add to cart functionality

--- lec_12/src/components/RestauranItems.js
+++ lec_12/src/components/RestauranItems.js
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import { CDN_URL } from "../utils/constant";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addItem } from "../redux/cartSlice";
-const RestaurantItems = ({itemsCards}) => {
+
+const RestaurantItems = ({ itemsCards }) => {
 
 
   const menuItemData = itemsCards.map((x) => x.card.info) || [];
   const dispatch = useDispatch();//it can't call inside the func hence we need to initialise and use it
- const handleAddItems = (item)=>{
-    dispatch(addItem(item));
-   }
- {/*
+  const items = useSelector((store) => store.cart.items);
+  const handleAddItems = (item) => {
+    console.log(items);
+    const isAlreadyInCart = items.some((cartItem) => cartItem.id === item.id);
+
+  if (isAlreadyInCart) {
+    alert("Item is already in the cart!");
+    return; // Don't dispatch if duplicate
+  }
+
+  dispatch(addItem(item));
+
+  }
+
+  {/*
   dispatch(actionName(playload))
   import useDispatch & action = addItem
 
@@ -19,37 +31,37 @@ const RestaurantItems = ({itemsCards}) => {
   payload : "pizza";
   }
  */}
-return(
-  <div className="">
-            {menuItemData.map((item) => (
-              <div className="menu-item" key={item?.id}>
-                <div className="menu-item-details">
-                  <h3 className="item-title">{item?.name}</h3>
-                  <p className="item-cost">
-                    {item?.price > 0
-                      ? new Intl.NumberFormat("en-IN", {
-                          style: "currency",
-                          currency: "INR",
-                        }).format(item?.price / 100)
-                      : " "}
-                  </p>
-                  <p className="item-desc">{item?.description}</p>
-                </div>
-                <div className="menu-img-wrapper">
-                  {item?.imageId && (
-                    <img
-                      className="menu-item-img"
-                      src={CDN_URL + item?.imageId}
-                      alt={item?.name}
-                    />
-                  )}
-                  <button className="add-btn" onClick={() =>handleAddItems(item)}> ADD +</button>
-                </div>
-              </div>
-            ))}
+  return (
+    <div className="">
+      {menuItemData.map((item) => (
+        <div className="menu-item" key={item?.id}>
+          <div className="menu-item-details">
+            <h3 className="item-title">{item?.name}</h3>
+            <p className="item-cost">
+              {item?.price > 0
+                ? new Intl.NumberFormat("en-IN", {
+                  style: "currency",
+                  currency: "INR",
+                }).format(item?.price / 100)
+                : " "}
+            </p>
+            <p className="item-desc">{item?.description}</p>
           </div>
-  
-)
+          <div className="menu-img-wrapper">
+            {item?.imageId && (
+              <img
+                className="menu-item-img"
+                src={CDN_URL + item?.imageId}
+                alt={item?.name}
+              />
+            )}
+            <button className="add-btn" onClick={() => handleAddItems(item)}> ADD +</button>
+          </div>
+        </div>
+      ))}
+    </div>
+
+  )
 };
 
 export default RestaurantItems;
